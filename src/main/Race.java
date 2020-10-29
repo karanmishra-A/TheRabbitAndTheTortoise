08b13a8f0ac71f23f7b73b8341b47f3fb8694fe0
karan <karan.mishra_cs19@gla.ac.in>
2020-10-29 14:27:25 +0530

    import the packages

--- src/main/Race.java
+++ src/main/Race.java
@@ -1,10 +1,30 @@
 package main;
 
 import thread.rabbit.Rabbit;
+import thread.tortoise.Tortoise;
+import javax.swing.*;
+import static java.lang.Thread.sleep;
 
-public class Race {
-    public static void main(String[] args) {
+
+public class Race<winner> {
+    public static void main(String[] args) throws InterruptedException {
         Rabbit rabbit = new Rabbit();
         rabbit.getRabbit().start();
+        Tortoise tortoise = new Tortoise();
+        tortoise.getTortoise().start();
+        String winner = null;
+        sleep(20000L);
+        {
+            if (tortoise.getTimeTakenByTortoise() < rabbit.getTimeTakenByRabbit()) {
+                winner = " Tortoise wins the race!";
+            }
+            else if (tortoise.getTimeTakenByTortoise() == rabbit.getTimeTakenByRabbit()) {
+                winner = "No one wins the race!";
+            }
+            else {
+                winner = "Rabbit wins the race";
+            }
+            JOptionPane.showMessageDialog(null, winner);
+        }
     }
 }
